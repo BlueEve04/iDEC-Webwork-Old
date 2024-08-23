3dd9a2b2b382753def665ae10ed781d30f7e5bae
BlueEve04 <blueeve04@qq.com>
2024-08-23 19:03:30 +0800

    frag_week1

---
+++ 0816/ex1/js.js
@@ -0,0 +1,12 @@
+// const box = document.querySelector('.scroll-container');
+
+//         // Add a click event listener to the box
+//         box.addEventListener('click', function() {
+//             // Select the resizable element inside the box
+//             const resizableElement = box.querySelector('.card');
+//             for(i=1;i<=2;i++){
+//                 // Change the size of the resizable element
+//             resizableElement.style.width = '150px';
+//             resizableElement.style.height = '75px';
+//             }
+//         });
